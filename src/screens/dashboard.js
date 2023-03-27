cafe79fc88de53505cb2d767d114ab9e7c4f842b
“naseer <naseer@edatasolutions.com.au>
2023-03-28 06:06:16 +1100

    get data from firebaseDb and show in flatlist

--- src/screens/dashboard.js
+++ src/screens/dashboard.js
@@ -1,25 +1,57 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, FlatList, Image } from "react-native";
 import { Button } from "@react-native-material/core";
-import React from 'react'
-import {setUserLoggedIn,getUserLoggedIn} from '../utils/helper'
+import React, { useEffect, useState } from "react";
+import { setUserLoggedIn, getUserLoggedIn } from "../utils/helper";
+import { collection, getDocs } from "firebase/firestore";
+import { auth, db, storage } from "../config/auth/firebase";
 
-export default function Dashboard({navigation}) {
+export default function Dashboard({ navigation }) {
+  const [users, setUsers] = useState();
 
-    const loggOut = () =>{
-        setUserLoggedIn({})
-        navigation.replace('LoginScreen')
-    }
+  useEffect(() => {
+    // console.log('dashboard')
+
+    (async function getDataFromDB() {
+      const usersDataArray = [];
+      const queryResultList = await getDocs(collection(db, "eatoos"));
+      queryResultList.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, " => ", doc.data());
+        usersDataArray.push(doc.data());
+      });
+
+      setUsers(usersDataArray);
+    })();
+  }, []);
+
+  const myRenderView = ({ item }) => {
+    return (
+      <View>
+        <Image source={{ uri: item.profileImageUrl }} style={{ height: 50 }} />
+        <Text>uid: {item.uid}</Text>
+        <Text>First Name: {item.fname}</Text>
+        <Text>Last Name: {item.lname}</Text>
+        <Text>Email: {item.email}</Text>
+      </View>
+    );
+  };
+
+  const loggOut = () => {
+    setUserLoggedIn({});
+    navigation.replace("LoginScreen");
+  };
   return (
-    <View>
+    <View style={{ paddingHorizontal: 10 }}>
       <Text>Dashboard</Text>
       <Button
-                style={{ margin: 16 }}
-                color="orange"
-                onPress={()=>loggOut()}
-                title="Logout"
-              />
+        style={{ margin: 16 }}
+        color="orange"
+        onPress={() => loggOut()}
+        title="Logout"
+      />
+      <FlatList data={users} renderItem={myRenderView} />
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
