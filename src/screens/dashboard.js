324d628afcee1043b9986dacac8d2f51e01e0df3
“naseer <naseer@edatasolutions.com.au>
2023-03-30 18:20:47 +1100

    in dashboard getbyid is added

--- src/screens/dashboard.js
+++ src/screens/dashboard.js
@@ -2,26 +2,49 @@ import { StyleSheet, Text, View, FlatList, Image } from "react-native";
 import { Button } from "@react-native-material/core";
 import React, { useEffect, useState } from "react";
 import { setUserLoggedIn, getUserLoggedIn } from "../utils/helper";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, doc, getDoc } from "firebase/firestore";
 import { auth, db, storage } from "../config/auth/firebase";
 
 export default function Dashboard({ navigation }) {
   const [users, setUsers] = useState();
 
+
+
+   const getDataFromDB = async()=>{
+    const usersDataArray = [];
+    const queryResultList = await getDocs(collection(db, "eatoos"));
+    queryResultList.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+      usersDataArray.push(doc.data());
+    });
+
+    setUsers(usersDataArray);
+  }
+
+
+
+
+  const getSingleData = async () => {
+    console.log("single data");
+    // give id and it will return from collection
+    const docRef = doc(db, "eatoos", "JWex0JePQIg0f0IVAC0gisLOUNr2");
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      console.log("Document data:", (docSnap.data()));
+    } else {
+      // doc.data() will be undefined in this case
+      alert("No such document!");
+    }
+  };
+
+
   useEffect(() => {
     // console.log('dashboard')
+    getDataFromDB();
 
-    (async function getDataFromDB() {
-      const usersDataArray = [];
-      const queryResultList = await getDocs(collection(db, "eatoos"));
-      queryResultList.forEach((doc) => {
-        // doc.data() is never undefined for query doc snapshots
-        console.log(doc.id, " => ", doc.data());
-        usersDataArray.push(doc.data());
-      });
-
-      setUsers(usersDataArray);
-    })();
+    
   }, []);
 
   const myRenderView = ({ item }) => {
@@ -49,7 +72,10 @@ export default function Dashboard({ navigation }) {
         onPress={() => loggOut()}
         title="Logout"
       />
+
+      <Text>Get all data from firebase</Text>
       <FlatList data={users} renderItem={myRenderView} />
+      <Button title="Get signle item from DB" onPress={getSingleData} />
     </View>
   );
 }
